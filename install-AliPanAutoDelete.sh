5022a52cd9a1ed1e1507f53d2b6c699bf84bd8c0
HanGK <99523510+Han-GK@users.noreply.github.com>
2023-08-23 08:03:56 +0800

    Update install-AliPanAutoDelete.sh

--- install-AliPanAutoDelete.sh
+++ install-AliPanAutoDelete.sh
@@ -14,13 +14,13 @@ mkdir -p /etc/autodel
 echo "目录创建完成，默认路径/etc/autodel"
 sleep 1
 echo "开始wget脚本文件"
-wget https://jihulab.com/HanGK/AliPanAutoDelete/-/blob/6280cfcd32fcd63554e1e787099f4c5dc0abeee9/main.py -P /etc/autodel/
+wget https://raw.githubusercontent.com/Han-GK/AliPanAutoDelete/main/main.py -P /etc/autodel/
 sleep 1
 echo "开始安装aligo依赖"
 pip3 install aligo
 sleep 3
 echo "开始设置定时"
-(crontab -l; echo "*/$interval * * * * /usr/bin/python3 /etc/autodel/test.py") | crontab -
+(crontab -l; echo "*/$interval * * * * /usr/bin/python3 /etc/autodel/main.py") | crontab -
 /usr/bin/python3 /etc/autodel/test.py &
 echo "请打开IP:33333扫码登录"
 echo "扫码后Ctrl+c手动退出"
